c093dd685af3d91a21a7226c5eb30a22659902a0
Carlos Barcelo <cbarcelo@usc.edu>
2023-04-12 20:45:38 +0000

    done

--- schedwork.cpp
+++ schedwork.cpp
@@ -20,7 +20,14 @@ static const Worker_T INVALID_ID = (unsigned int)-1;
 
 
 // Add prototypes for any helper functions here
-
+bool assignWorkers(
+    const AvailabilityMatrix& avail,
+    const size_t dailyNeed,
+    const size_t maxShifts,
+    DailySchedule& sched,
+    size_t day,
+    std::vector<size_t>& shifts
+);
 
 // Add your implementation of schedule() and other helper functions here
 
@@ -31,14 +38,59 @@ bool schedule(
     DailySchedule& sched
 )
 {
-    if(avail.size() == 0U){
+    	const size_t numWorkers = avail[0].size();
+    const size_t numDays = avail.size();
+    const size_t numShifts = dailyNeed * numDays;
+
+    // check if it is possible to schedule the workers
+    if (numShifts > numWorkers * maxShifts) {
         return false;
     }
+
+    // initialize the schedule and shifts counters
     sched.clear();
-    // Add your code below
+    sched.resize(numDays, std::vector<Worker_T>(dailyNeed));
+    std::vector<size_t> shifts(numWorkers, 0);
 
+    // call the recursive algorithm
+    return assignWorkers(avail, dailyNeed, maxShifts, sched, 0, shifts);
+}
 
+bool assignWorkers(
+    const AvailabilityMatrix& avail,
+    const size_t dailyNeed,
+    const size_t maxShifts,
+    DailySchedule& sched,
+    size_t day,
+    std::vector<size_t>& shifts
+) {
+    const size_t numWorkers = avail[0].size();
+    const size_t numDays = avail.size();
 
+    // base case: we have scheduled d workers for each day
+    if (day == numDays) {
+        return true;
+    }
 
+    // try to assign a worker for this day
+    for (size_t w = 0; w < numWorkers; w++) {
+        if (avail[day][w] && shifts[w] < maxShifts) {
+            // worker w is available on this day and has not worked m shifts
+            sched[day].push_back(w);
+            shifts[w]++;
+
+            // recursive call on the next day
+            if (assignWorkers(avail, dailyNeed, maxShifts, sched, day+1, shifts)) {
+                return true;
+            }
+
+            // undo the assignment and try the next available worker
+            sched[day].pop_back();
+            shifts[w]--;
+        }
+    }
+
+    // backtrack to the previous day
+    return false;
 }
 
--- wordle.cpp
+++ wordle.cpp
@@ -13,7 +13,7 @@ using namespace std;
 
 
 // Add prototypes of helper functions here
-
+void nerdler( std::string in,  std::string floating, std::set<std::string>* words);
 
 // Definition of primary wordle function
 std::set<std::string> wordle(
@@ -21,8 +21,50 @@ std::set<std::string> wordle(
     const std::string& floating,
     const std::set<std::string>& dict)
 {
-    // Add your code here
+    std::set<std::string> words;
 
+    //wordler(in, floating, &words);
+
+    nerdler(in, floating, &words);
+
+    std::set<std::string> valid;
+
+    std::set<std::string>::iterator it;
+
+
+    for(it = words.begin(); it != words.end(); it++){
+      if (dict.find(*it) != dict.end()){
+        valid.insert(*it);
+      }
+    }
+
+  return valid;
 }
 
 // Define any helper functions here
+
+void nerdler( std::string in,  std::string floating, std::set<std::string>* words){
+
+  //std::cout << in << std::endl;
+
+
+  for(size_t i = 0; i < in.length(); i++){
+    std::string sin = in;
+    if(in[i] == '-' && floating.length() > 0){
+        sin[i] = floating[floating.length()-1];
+        std::string fl = floating;
+        fl.pop_back();
+        nerdler(sin, fl, words);
+    }
+    else if(in[i] == '-'){
+      for(char c = 'a'; c <= 'z'; c++){
+        sin[i] = c;
+        nerdler(sin, floating, words);
+      }
+    }
+
+    if(i == in.length()-1) words->insert(sin);
+  }
+}
+
+
